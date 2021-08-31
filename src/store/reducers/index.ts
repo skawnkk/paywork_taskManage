1b58fe5057ba3a43bc52337173ad2c407dbb4634
Namju <skawn11114@gmail.com>
2021-08-31 15:53:18 +0900

    chore. 주석정리 및 불필요 파일 삭제

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,6 +1,5 @@
 import { combineReducers } from "redux";
 import tasks from "./tasks";
-// import tasks from "store/reducers/tasks";
 const rootReducer = combineReducers({ tasks });
 
 export default rootReducer;
